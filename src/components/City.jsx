7860cf6af308c21d3a744b5698bdc5c6b6e428c7
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-02 16:26:15 +0300

    added lazy loading

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -23,7 +23,7 @@ function City() {
 
   useEffect(() => {
     getCity(id);
-  }, [id]);
+  }, [id, getCity]);
 
   const { cityName, emoji, date, notes } = currentCity;
 
--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useCallback, useContext, useEffect, useReducer } from "react";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -70,7 +70,7 @@ function CitiesProvider({ children }) {
     loadCitites();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(async function getCity(id) {
     if (Number(id) === currentCity.id) return;
 
     try {
@@ -86,7 +86,7 @@ function CitiesProvider({ children }) {
         payload: "There was an error loading city...",
       });
     }
-  }
+  }, [currentCity.id]);
 
   async function createCity(newCity) {
     try {
